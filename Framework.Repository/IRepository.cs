583a5d17d2c9de85d419dbb24b143d43b397226a
Shoniz® IT Dept <44108572+ITShoniz@users.noreply.github.com>
2018-10-14 22:42:39 +0330

    Add files via upload

---
+++ Framework.Repository/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Framework.Repository
+{
+    public interface IRepository<T>
+    {
+        T GetById(Int64 id);
+
+        void Create(T entity);
+
+        void Delete(T entity);
+
+        void Update(T entity);
+
+        List<T> GetAll(int Top);
+    }
+}
