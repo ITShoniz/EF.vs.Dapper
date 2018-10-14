ed29e401cdf5db64a7af16a2ca0570b98c2a898d
Shoniz® IT Dept <44108572+ITShoniz@users.noreply.github.com>
2018-10-14 22:46:08 +0330

    Add files via upload

---
+++ Repository/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Repository
+{
+    public interface IRepository<T>
+    {
+        T GetById(Int64 id);
+
+        void Create(T entity);
+
+        void Delete(T entity);
+
+        void Update(T entity);
+    }
+}
